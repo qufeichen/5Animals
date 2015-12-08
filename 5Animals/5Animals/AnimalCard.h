a50363ea9399c17cce288d01e73027293c8408d7
Qufei Chen <qufei.chen@gmail.com>
2015-12-07 23:09:29 -0500

    defined ostream and istream operators for AnimalCard

--- 5Animals/5Animals/AnimalCard.h
+++ 5Animals/5Animals/AnimalCard.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #ifndef ANIMALCARD_H
 #define ANIMALCARD_H
 
@@ -33,6 +34,11 @@ enum Orientation{
 class AnimalCard{
     
 private:
+    Orientation orientation;
+    EvenOdd evenOdd;
+    EvenOdd next;
+    char cardMatrix[2][2];
+    char topLeft, topRight, bottomLeft, bottomRight;
 
 public:
 
@@ -43,7 +49,54 @@ public:
 	virtual void printRow( ostream &, EvenOdd )=0;
     virtual char getAnimal(int, int)=0;
 	virtual void printRow(EvenOdd )=0;
-	//virtual friend ostream & operator <<(ostream &out, const AnimalCard&) = 0;
+    
+    inline friend ostream & operator <<(ostream &out, const AnimalCard& card){
+        
+        out << card.orientation << endl;
+        out << card.evenOdd << endl;
+        out << card.cardMatrix[0][0] << endl;
+        out << card.cardMatrix[0][1] << endl;
+        out << card.cardMatrix[1][0] << endl;
+        out << card.cardMatrix[1][1] << endl;
+        out << card.topLeft << endl;
+        out << card.topRight << endl;
+        out << card.bottomLeft << endl;
+        out << card.bottomRight << endl;
+        
+        return out;
+        
+    }
+    
+    inline friend istream & operator >>(istream &in, AnimalCard& card){
+        
+        string o;
+        in >> o;
+        if(o == "UP"){
+            card.orientation = Orientation::UP;
+        } else {
+            card.orientation = Orientation::DOWN;
+        }
+        string eo;
+        in >> eo;
+        if(eo == "EVEN"){
+            card.evenOdd = EvenOdd::EVEN;
+            card.next = EvenOdd::ODD;
+        } else {
+            card.evenOdd = EvenOdd::ODD;
+            card.next = EvenOdd::EVEN;
+        }
+        
+        in>>card.cardMatrix[0][0];
+        in>>card.cardMatrix[0][1];
+        in>>card.cardMatrix[1][0];
+        in>>card.cardMatrix[1][1];
+        in >> card.topLeft;
+        in >> card.topRight;
+        in >> card.bottomLeft;
+        in >> card.bottomRight;
+        
+        return in;
+    }
 
 };
 
