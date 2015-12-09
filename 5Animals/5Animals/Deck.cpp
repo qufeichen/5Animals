cc4b44cb71fa1bb19e3ec8210390529ecf6845a6
Qufei Chen <qufei.chen@gmail.com>
2015-12-09 13:24:29 -0500

    removed empty deck.cpp file

--- 5Animals/5Animals/Deck.cpp
+++
@@ -1,6 +0,0 @@
-//5Animals
-//CSI2372 Final Project
-//Professor Jochen Lang
-//Qufei Chen - 6771326
-//Haoyuan Luo - 6838050
-//Due Date: December 9th, 2015
--- 5Animals/5Animals/Deck.h
+++ 5Animals/5Animals/Deck.h
@@ -32,5 +32,5 @@ public:
     }
 
 };
-#include "Deck.cpp"
+
 #endif
